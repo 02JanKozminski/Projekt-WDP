eb4438021542d2cc37543d83a1b19fc3f23cb25f
02JanKozminski <107435367+02JanKozminski@users.noreply.github.com>
2022-06-13 22:19:20 +0200

    Add files via upload

---
+++ Projekt-WDP/test - Kopia/Unit1.cpp
@@ -0,0 +1,384 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+char wynik[20];
+int j = 0;
+ofstream plik("plik.txt", ios::app);
+
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+
+template <class T>
+class Stack{
+    int maxSize;
+    T *tab;
+    int top;
+    
+    public:
+        Stack( int size ){
+            maxSize = size;
+            tab = new T[ size ];
+            top = -1;
+        }
+        
+        ~Stack(){
+            delete []tab;
+        }
+        
+        void push( T s ){
+            tab[ ++top ] = s;
+        }
+        
+        T pop(){
+            return tab[top--];
+        }
+        
+        int size(){
+            return top + 1;
+        }
+        
+        bool empty(){
+            return top == -1;
+        }
+        
+        bool isFull(){
+            return top == maxSize - 1;
+        }
+        
+        T peek(){
+            return tab[top];
+        }
+        
+        void erase(){
+            top = -1;
+        }
+};
+
+int prior( char z ){
+    if( z=='+' || z=='-' ) return 1;
+    if( z=='*' || z=='/' || z=='%' ) return 2;
+    if( z=='^' ) return 3;
+    if( z=='.' ) return 4;
+    return 0;
+}
+
+float equation( float a, float b, char op ){ 
+    
+    string result = "";
+    double w;
+    stringstream ss1;
+    stringstream ss2;
+    ss1 << a;
+    ss2 << b;
+
+    int one;
+    int two;
+    
+    switch( op ){
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        case '^': return pow(a, b);
+        case '%': one = a; two = b; return one % two;
+        case '.': result += ss1.str() + "." + ss2.str(); w = atof(result.c_str()); return w;
+    }
+    return 0;
+}
+
+float calculation( char symbol[] ){
+    unsigned int i=0;
+    
+    Stack <float> values(200);                 
+    Stack <char> ops(200);
+    
+    for ( i = 0; i < strlen(symbol); i++) {
+        if( symbol[i] == ' ' )
+            continue;
+        
+        else if( symbol[i] == '(' ){
+            ops.push( symbol[i] );
+        }
+        
+        else if( isdigit(symbol[i])){   
+            float val = 0;
+            
+            while( i<strlen(symbol) && isdigit(symbol[i]) ){
+                val = (val*10) + (symbol[i]-'0');
+                i++;
+            }
+            values.push(val);
+            i--;
+        }
+        
+        
+        else if( symbol[i] == ')' ){
+            while( !ops.empty() && ops.peek() != '(' ){
+                float val2 = values.peek();
+                values.pop();
+                
+                float val1 = values.peek();
+                values.pop();
+                
+                char op = ops.peek();
+                ops.pop();
+                
+                values.push( equation(val1, val2, op) );
+            }
+            if( !ops.empty() )
+                ops.pop();
+        }
+        else{
+            if( symbol[i] == '-' && isdigit( symbol[i+1] ) ){
+                float val = 0;
+                while( i+1 < strlen(symbol) && isdigit(symbol[i+1]) ){
+                    val = (val*10) + (symbol[i+1] - '0');
+                    i++;
+                }
+                val = -val;
+                values.push(val);
+                continue;
+            }
+            
+            while( !ops.empty() && prior(ops.peek()) >= prior(symbol[i]) ){  
+                float val2 = values.peek();
+                values.pop();
+                
+                float val1 = values.peek();
+                values.pop();
+                
+                char op = ops.peek();
+                ops.pop();
+                
+                values.push( equation(val1, val2, op) );
+            }
+            ops.push(symbol[i]);
+            
+        }
+        
+    }
+    
+    while( !ops.empty() ){
+        float val2 = values.peek();
+        values.pop();
+                
+        float val1 = values.peek();
+        values.pop();
+                
+        char op = ops.peek();
+        ops.pop();
+                
+        values.push( equation(val1, val2, op) );    
+    }
+    
+    return values.peek();
+}
+
+void __fastcall TForm1::FormCreate(TObject *Sender)
+{
+        Image1->Picture->LoadFromFile("Numery/z7.bmp");
+        Image2->Picture->LoadFromFile("Numery/z8.bmp");
+        Image3->Picture->LoadFromFile("Numery/z9.bmp");
+        Image4->Picture->LoadFromFile("Numery/z4.bmp");
+        Image5->Picture->LoadFromFile("Numery/z5.bmp");
+        Image6->Picture->LoadFromFile("Numery/z6.bmp");
+        Image7->Picture->LoadFromFile("Numery/z1.bmp");
+        Image8->Picture->LoadFromFile("Numery/z2.bmp");
+        Image9->Picture->LoadFromFile("Numery/z3.bmp");
+        Image10->Picture->LoadFromFile("Numery/z0.bmp");
+        Image11->Picture->LoadFromFile("Numery/z..bmp");
+        Image12->Picture->LoadFromFile("Numery/zce.bmp");
+        Image13->Picture->LoadFromFile("Numery/z(.bmp");
+        Image14->Picture->LoadFromFile("Numery/z).bmp");
+        Image15->Picture->LoadFromFile("Numery/z+.bmp");
+        Image16->Picture->LoadFromFile("Numery/z-.bmp");
+        Image17->Picture->LoadFromFile("Numery/zm.bmp");
+        Image18->Picture->LoadFromFile("Numery/zd.bmp");
+        Image19->Picture->LoadFromFile("Numery/z^.bmp");
+        Image20->Picture->LoadFromFile("Numery/z=.bmp");
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image1Click(TObject *Sender)
+{
+        wynik[j++] += '7';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image2Click(TObject *Sender)
+{
+        wynik[j++] += '8';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image3Click(TObject *Sender)
+{
+        wynik[j++] += '9';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image4Click(TObject *Sender)
+{
+        wynik[j++] += '4';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image5Click(TObject *Sender)
+{
+        wynik[j++] += '5';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image6Click(TObject *Sender)
+{
+        wynik[j++] += '6';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image7Click(TObject *Sender)
+{
+        wynik[j++] += '1';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image8Click(TObject *Sender)
+{
+        wynik[j++] += '2';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image9Click(TObject *Sender)
+{
+        wynik[j++] += '3';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image10Click(TObject *Sender)
+{
+        wynik[j++] += '0';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image11Click(TObject *Sender)
+{
+        wynik[j] = '.';
+        j++;
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image12Click(TObject *Sender)
+{
+        for(int i=0; i<20; i++){
+                wynik[i] = NULL;
+        }
+        j=0;
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image13Click(TObject *Sender)
+{
+        wynik[j++] += ' ';
+        wynik[j++] += '(';
+        wynik[j++] += ' ';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image14Click(TObject *Sender)
+{
+        wynik[j++] += ' ';
+        wynik[j++] += ')';
+        wynik[j++] += ' ';
+        Label1->Caption = wynik;
+}
+
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image15Click(TObject *Sender)
+{
+        wynik[j++] += ' ';
+        wynik[j++] += '+';
+        wynik[j++] += ' ';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image16Click(TObject *Sender)
+{
+        if(wynik[j-1] >= '1' && wynik[j-1] <='9'){
+                wynik[j++] += ' ';
+                wynik[j++] += '-';
+                wynik[j++] += ' ';
+                Label1->Caption = wynik;
+        }
+        else{
+                wynik[j++] += '-';
+                Label1->Caption = wynik;
+        }
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image17Click(TObject *Sender)
+{
+        wynik[j++] += ' ';
+        wynik[j++] += '*';
+        wynik[j++] += ' ';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image18Click(TObject *Sender)
+{
+        wynik[j++] += ' ';
+        wynik[j++] += '/';
+        wynik[j++] += ' ';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image19Click(TObject *Sender)
+{
+        wynik[j++] += ' ';
+        wynik[j++] += '^';
+        wynik[j++] += ' ';
+        Label1->Caption = wynik;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image20Click(TObject *Sender)
+{
+        Label1->Caption = CurrToStr(calculation(wynik));
+
+        plik << wynik << " = " << calculation(wynik) << endl;
+}
+//---------------------------------------------------------------------------
+
---
+++ Projekt-WDP/test - Kopia/Unit1.~dfm
@@ -0,0 +1,169 @@
+object Form1: TForm1
+  Left = 864
+  Top = 174
+  Width = 352
+  Height = 428
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Image1: TImage
+    Left = 16
+    Top = 128
+    Width = 49
+    Height = 49
+    OnClick = Image1Click
+  end
+  object Image2: TImage
+    Left = 80
+    Top = 128
+    Width = 49
+    Height = 49
+    OnClick = Image2Click
+  end
+  object Image3: TImage
+    Left = 144
+    Top = 128
+    Width = 49
+    Height = 49
+    OnClick = Image3Click
+  end
+  object Image4: TImage
+    Left = 16
+    Top = 192
+    Width = 49
+    Height = 49
+    OnClick = Image4Click
+  end
+  object Image5: TImage
+    Left = 80
+    Top = 192
+    Width = 49
+    Height = 49
+    OnClick = Image5Click
+  end
+  object Image6: TImage
+    Left = 144
+    Top = 192
+    Width = 49
+    Height = 49
+    OnClick = Image6Click
+  end
+  object Image7: TImage
+    Left = 16
+    Top = 256
+    Width = 49
+    Height = 49
+    OnClick = Image7Click
+  end
+  object Image8: TImage
+    Left = 80
+    Top = 256
+    Width = 49
+    Height = 49
+    OnClick = Image8Click
+  end
+  object Image9: TImage
+    Left = 144
+    Top = 256
+    Width = 49
+    Height = 49
+    OnClick = Image9Click
+  end
+  object Image10: TImage
+    Left = 16
+    Top = 320
+    Width = 49
+    Height = 49
+    OnClick = Image10Click
+  end
+  object Image11: TImage
+    Left = 80
+    Top = 320
+    Width = 49
+    Height = 49
+    OnClick = Image11Click
+  end
+  object Image12: TImage
+    Left = 144
+    Top = 320
+    Width = 49
+    Height = 49
+    OnClick = Image12Click
+  end
+  object Image13: TImage
+    Left = 208
+    Top = 128
+    Width = 49
+    Height = 49
+    OnClick = Image13Click
+  end
+  object Image14: TImage
+    Left = 272
+    Top = 128
+    Width = 49
+    Height = 49
+    OnClick = Image14Click
+  end
+  object Image15: TImage
+    Left = 208
+    Top = 192
+    Width = 49
+    Height = 49
+    OnClick = Image15Click
+  end
+  object Image16: TImage
+    Left = 272
+    Top = 192
+    Width = 49
+    Height = 49
+    OnClick = Image16Click
+  end
+  object Image17: TImage
+    Left = 208
+    Top = 256
+    Width = 49
+    Height = 49
+    OnClick = Image17Click
+  end
+  object Image18: TImage
+    Left = 272
+    Top = 256
+    Width = 49
+    Height = 49
+    OnClick = Image18Click
+  end
+  object Image19: TImage
+    Left = 208
+    Top = 320
+    Width = 49
+    Height = 49
+    OnClick = Image19Click
+  end
+  object Image20: TImage
+    Left = 272
+    Top = 320
+    Width = 49
+    Height = 49
+    OnClick = Image20Click
+  end
+  object Label1: TLabel
+    Left = 16
+    Top = 16
+    Width = 305
+    Height = 97
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -35
+    Font.Name = 'Times New Roman'
+    Font.Style = []
+    ParentFont = False
+  end
+end
